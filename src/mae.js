9d3394977aabef04c0702203f86b5675d00cc2bd
Rob Griffiths <rob@bytespider.eu>
2017-05-30 12:34:56 +0100

    Optimise

--- src/mae.js
+++ src/mae.js
@@ -3,9 +3,10 @@ module.exports = function mae(original, noisy, step) {
     var offset;
     var sum = 0;
     var i = 0;
+    var l = original.length;
     step = step || 1;
 
-    for (i = 0; i < original.length; i += step) {
+    for (i = 0; i < l; i += step) {
         error = Math.abs(original[i] - noisy[i]);
         sum += error;
     }
